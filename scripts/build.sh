27554f6e511469a5f50935f1e752a0d3c8825f70
sonu <sonumandloi7873@gmail.com>
2026-06-04 20:04:16 +0530

    change in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 IMAGE_NAME="sonu7873/flask-app-devops"
-IMAGE_TAG=$BUILD_NUMBER
+# IMAGE_TAG=$BUILD_NUMBER
 
 echo "Building image: $IMAGE_NAME:$IMAGE_TAG"
 
-docker build -t $IMAGE_NAME:$IMAGE_TAG -f app/DockerFile app
+docker build -t $IMAGE_NAME:latest -f app/DockerFile app
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
